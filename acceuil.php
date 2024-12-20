b1591793a199bb94f745ccd71f4000b8e1062628
Samuel IKENGE BENKOMA <samuel.ikengebenkoma@edouardgand.fr>
2024-12-20 14:46:56 +0100

    first commit

---
+++ acceuil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+    <title>Société Lafleur - Accueil</title>
+    <meta name="description" content="Accueil LaFLEUR" >
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+    <meta http-equiv="Content-Language" content="fr" >
+    <!-- Fichier : accueil.php -->
+    <!-- Auteur : Almy Léo -->
+</head>
+<body>
+    <?php
+    include('menu.php');
+    ?>
+
+    <h2>"Dites-le avec Lafleur"</h2>
+    <p><img src="Images/accueil.JPG" alt="Photo de fleur (accueil.jpg)" width="300"></p>
+
+    <p>Appelez notre service commercial au <strong>03.22.84.65.74</strong> pour recevoir un bon de commande</p>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<p><a href="accueil.php">Accueil</a></p>
+    <hr>
+    <ul>
+        <li><a href="listePdt.php?idCateg=b">Bulbes</a></li>
+        <li><a href="listePdt.php?idCateg=r">Rosiers</a></li>
+        <li><a href="listePdt.php?idCateg=m">Plantes à massifs</a></li>
+    </ul>
